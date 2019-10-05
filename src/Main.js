ac789b190e0db7f6e632cd14b3865737c2edfe81
Shamma Huraiz <shamma.alhuraiz@hotmail.com>
2019-10-05 15:51:00 +0400

    continue Enter

--- src/Main.js
+++ src/Main.js
@@ -1,72 +1,3 @@
-<<<<<<< HEAD
-import React, { useState, useEffect }  from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './Home';
-import About from './About';
-import Contact from './Contact'
-import NavBar from './NavBar'
-import AppContext from './AppContext.js'
-
-const Main = () => {
-
-    const [globalState, setGlobalState] = useState(
-        { 
-            loggedIn: localStorage.getItem('token') ? true : false,
-            userForm: null,
-            users: [
-              'Jim',
-              'Jane',
-              'Mohammad',
-              'Mary'
-            ],
-            feed: [
-              {user: 'Jim', comment: 'This comment is from Jim'},
-              {user: 'Jane', comment: 'Going to tennis practice tonight'},
-              {user: 'Mohammad', comment: 'Big traffic near dubai'},
-              {user: 'Mary', comment: 'Graduating in 1 week! #exicted'},
-            ]
-        }
-    );
-
-    useEffect(()=>{
-        if(!localStorage.getItem('token') && globalState.loggedIn) {
-            localStorage.setItem('token', globalState.token)
-        }
-
-        if(!globalState.loggedIn) {
-            localStorage.removeItem('token');
-        }
-    });
-
-    const LayoutRoute = ({ location, path, exact, component }) => {
-        return (
-            <div>
-                <NavBar 
-                    location={location.pathname} 
-                    logo="XYZ Company"/>
-                <Route path={path} exact={exact} component={component} />
-                <footer>
-                    2019 mywebsite.com
-                </footer>
-            </div>
-        )
-    }
-
-    return (  
-        <AppContext.Provider value={[globalState, setGlobalState]}>
-            <BrowserRouter>
-                <Switch>
-                    <LayoutRoute path="/" exact component={Home} />
-                    <LayoutRoute path="/about" component={About} />
-                    <LayoutRoute path="/contact" component={Contact} />
-                </Switch>
-            </BrowserRouter>
-        </AppContext.Provider>
-    )
-}
-
-export default Main;
-=======
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './App';
@@ -100,4 +31,3 @@ const Main = () => {
 export default Main;
 
 
->>>>>>> Setup for app.
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-import React, { useState, useContext, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-=======
 import React, { useContext } from 'react';
->>>>>>> Setup for app.
 import AppContext from './AppContext';
 
 const LoginGroup = () => {
@@ -13,17 +8,6 @@ const LoginGroup = () => {
         setGlobalState({ ...globalState, loggedIn: false })
     }
 
-<<<<<<< HEAD
-    const showRegistrationForm = () => {
-        setGlobalState({ ...globalState, userForm: 'registration' })
-    }
-
-    const showLoginForm = () => {
-        setGlobalState({ ...globalState, userForm: 'login' })
-    }
-
-=======
->>>>>>> Setup for app.
     if(globalState.loggedIn) {
         return (
             <div>
@@ -35,13 +19,8 @@ const LoginGroup = () => {
     } else {
         return (
             <div>
-<<<<<<< HEAD
-                <button onClick={showRegistrationForm} className="btn btn-primary">Register</button>
-                <button onClick={showLoginForm} className="btn btn-primary">Log In</button>
-=======
                 <button className="btn btn-primary">Register</button>
                 <button className="btn btn-primary">Log In</button>
->>>>>>> Setup for app.
             </div>
         )
     }
@@ -49,33 +28,6 @@ const LoginGroup = () => {
 }
 
 const NavBar = (prop) => {
-<<<<<<< HEAD
-
-    const [state, setState] = useState({
-        currentPage: prop.location,
-        home: prop.location === '/' ? 'btn-success' : 'btn-primary',
-        about: prop.location === '/about' ? 'btn-success' : 'btn-primary',
-        contact: prop.location === '/contact' ? 'btn-success' : 'btn-primary',
-    });
-
-    // Use useEffect to listen to changes outside of the component
-    useEffect(()=>{
-
-        // When the prop.location changes
-        if(prop.location !== state.currentPage) {
-
-            // Renew the state
-            setState({
-                currentPage: prop.location,
-                home: prop.location === '/' ? 'btn-success' : 'btn-primary',
-                about: prop.location === '/about' ? 'btn-success' : 'btn-primary',
-                contact: prop.location === '/contact' ? 'btn-success' : 'btn-primary',             
-            })
-        }
-    })
-
-=======
->>>>>>> Setup for app.
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div>
@@ -83,15 +35,9 @@ const NavBar = (prop) => {
             </div>
 
             <div className="btn-group">
-<<<<<<< HEAD
-              <Link to="/" className={`btn ${state.home}`}>Home</Link>
-              <Link to="/about" className={`btn ${state.about}`}>About</Link>
-              <Link to="/contact" className={`btn ${state.contact}`}>Contact</Link>
-=======
               <button className="btn btn-primary">Home</button>
               <button className="btn btn-primary">About</button>
               <button className="btn btn-primary">Contact</button>
->>>>>>> Setup for app.
             </div>
 
             <LoginGroup />
